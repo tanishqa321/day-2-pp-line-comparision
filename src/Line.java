666f245d189e18fb3c19eac2ba3d250b7014a991
tanishqa321 <tender.tanishka@gmail.com>
2024-12-28 08:12:15 +0530

    Added UC1

---
+++ src/Line.java
@@ -0,0 +1,13 @@
+public  class Line {
+    double x1,x2,y1,y2;
+    public Line(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public double calculateLength() {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Welcome to Line Comparison Computation Program");
 
+        System.out.println("Welcome to Line Comparison Computation Program");
+        System.out.println("Enter the coordinates for the first line (x1, y1, x2, y2): ");
+        Scanner scanner = new Scanner(System.in);
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        Line line1 = new Line(x1, y1, x2, y2);
+        System.out.println(line1.calculateLength());
+        scanner.close();
     }
 }
